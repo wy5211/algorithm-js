789ee7a7d57f25eca3c5d4adad4711c8e8fed33c
wy5211 <1579151657@qq.com>
2021-12-13 10:26:26 +0800

    add graph & dictionary

---
+++ src/Dictionary/index.js
@@ -0,0 +1,38 @@
+// 字典
+function Dictionary() {
+  this.items = {};
+
+  Dictionary.prototype.set = function (key, value) {
+    this.items[key] = value;
+  };
+
+  Dictionary.prototype.has = function (key) {
+    return this.items.hasOwnProperty(key);
+  };
+
+  Dictionary.prototype.remove = function (key) {
+    if (!this.has(key)) {
+      return false;
+    }
+    delete this.items[key];
+    return true;
+  };
+
+  Dictionary.prototype.get = function (key) {
+    return this.items[key] ? this.items[key] : undefined;
+  };
+
+  Dictionary.prototype.keys = function () {
+    return Object.keys(this.items);
+  };
+
+  Dictionary.prototype.values = function () {
+    return Object.values(this.items);
+  };
+
+  Dictionary.prototype.clear = function () {
+    this.items = {};
+  };
+}
+
+export default Dictionary;
---
+++ src/Graph/eg.js
@@ -0,0 +1,21 @@
+import Graph from './inde';
+
+const graph = new Graph();
+
+graph.addVertex('A');
+graph.addVertex('B');
+graph.addVertex('C');
+graph.addVertex('D');
+graph.addVertex('E');
+graph.addVertex('F');
+
+graph.addEdge('A', 'B')
+graph.addEdge('A', 'D')
+graph.addEdge('A', 'E')
+graph.addEdge('A', 'F')
+graph.addEdge('B', 'F')
+graph.addEdge('E', 'B')
+graph.addEdge('F', 'E')
+graph.addEdge('C', 'B')
+
+console.log(graph.toString());
