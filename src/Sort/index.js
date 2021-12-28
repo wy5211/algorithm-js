5c9261fffc3f70b9fad2d60f00a1d65b4202ea83
wy5211 <1579151657@qq.com>
2021-12-28 10:19:22 +0800

    冒泡排序

--- src/Sort/index.js
+++ src/Sort/index.js
@@ -19,17 +19,29 @@ class ArrayList {
     this.arrry[n] = temp;
   }
 
+  // 冒泡排序，复杂度：O(N2)
   bubbleSort() {
     const len = this.arrry.length;
-    for (let j = len - 1; j >= 0; j--) {
-      for (let i = 0; i < j; i++) {
-        if (this.arrry[i] > this.arrry[i + 1]) {
-          this.swap(i, i + 1);
+    // 写法一
+    // for (let j = len - 1; j >= 0; j--) {
+    //   for (let i = 0; i < j; i++) {
+    //     if (this.arrry[i] > this.arrry[i + 1]) {
+    //       this.swap(i, i + 1);
+    //     }
+    //   }
+    // }
+
+    // 写法二
+    for (let i = 0; i < len - 1; i++) {
+      for (let j = 0; j < len - 1 - i; j++) {
+        if (this.arrry[j] > this.arrry[j + 1]) {
+          this.swap(j, j + 1);
         }
       }
     }
+    
     return this.arrry;
-  }
+  } 
 }
 
 export default ArrayList;
